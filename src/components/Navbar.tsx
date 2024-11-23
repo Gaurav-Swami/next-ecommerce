9e45b8375c5e4ec6ac3be9526f77c8ba0d33a219
Gaurav-Swami <gauravswami0404@gmail.com>
2024-11-23 20:03:19 +0530

    progress on navbar :)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,18 @@
-import Link from "next/link"
-import Menu from "./Menu"
+import Link from "next/link";
+import Menu from "./Menu";
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-    
-    {/* MOBILE */}
-    <Link href={"/"}>
-      LAMA
-      <Menu/>
-    </Link>
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      {/* MOBILE */}
+      <div className="flex items-center justify-between h-full">
+        <Link href={"/"}>
+          <div className="text-2xl tracking-wide">LAMA</div>
+        </Link>
+          <Menu />
+      </div>
     </div>
+  );
+};
 
-
-    
-  )
-}
-
-export default Navbar
+export default Navbar;
